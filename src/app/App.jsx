3bdf11de64fe5009994cd08ca0373bc6b22a6dd8
Minhaz <sharif35-3001@diu.edu.bd>
2023-02-05 23:04:10 +0600

    feat: project completed

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import PostList from "../features/posts/PostList";
+import AddPostForm from "../features/posts/AddPostForm";
+
+function App() {
+	return (
+		<main className="App">
+			<AddPostForm />
+			<PostList />
+		</main>
+	);
+}
+
+export default App;
